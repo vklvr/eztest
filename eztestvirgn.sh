a81c41a6decd4059624b04682bb0213003d8b7f3
vikelvar <vikelvar@yandex-team.ru>
2021-05-13 08:51:47 +0200

    Первый коммит

---
+++ eztestvirgn.sh
@@ -0,0 +1,6 @@
+#Конфиг
+# alias eztestcfg='open !!ТВОЯ ДИРЕКТОРИЯ!!/eztestvirgn.sh' #Открывает этот файл
+
+#Прокси
+# alias proxon='adb shell settings put global http_proxy !!ВВЕДИ IP УСТРОЙСТВА ИЗ CHARLES!!' #Включает прокси для Charles
+alias proxoff='adb shell settings put global http_proxy :0' #Отключает прокси
